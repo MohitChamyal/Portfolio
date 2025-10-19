572db8ffdad8e2a8e649d7f840dc192816986555
Mohit Chamyal <mohitchamyal58@gmail.com>
2025-10-19 20:19:37 +0530

    Fix CORS origin for Vercel frontend

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://mohitchamyal-portfolio.vercel.app/'
+    'https://mohitchamyal-portfolio.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
